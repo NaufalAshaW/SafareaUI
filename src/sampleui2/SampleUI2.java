30d8182122680a4413ce839bd1431a6bb7eff84f
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-12-16 17:28:13 +0700

    Add files via upload

---
+++ src/sampleui2/SampleUI2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleui2;
+
+/**
+ *
+ * @author Naufal Asha
+ */
+public class SampleUI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RegisterFrame rf = new RegisterFrame();
+        rf.setVisible(true);
+        rf.setLocationRelativeTo(null);
+    }
+    
+}
